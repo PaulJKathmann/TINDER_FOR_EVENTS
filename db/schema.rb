6eef90ee81876fe288a19a3780fc4ba7160a9cbb
Lu Zhu <luzhu000@gmail.com>
2019-07-17 18:02:36 +0200

    basic_sign_in_up_form + logo

--- db/schema.rb
+++ db/schema.rb
@@ -85,15 +85,15 @@ ActiveRecord::Schema.define(version: 2019_07_17_123723) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "name"
-    t.string "bio"
-    t.integer "age"
-    t.string "gender"
-    t.string "preferred_gender"
-    t.string "preferred_match_type"
-    t.string "role", default: "user"
-    t.integer "upper_age_preference"
-    t.integer "lower_age_preference"
+    t.string "name", default: "Mr. Tester", null: false
+    t.string "bio", default: " "
+    t.integer "age", default: 21, null: false
+    t.string "gender", default: "N/A", null: false
+    t.string "preferred_gender", default: "all", null: false
+    t.string "preferred_match_type", default: "Friends", null: false
+    t.string "role", default: "user", null: false
+    t.integer "upper_age_preference", default: 18, null: false
+    t.integer "lower_age_preference", default: 60, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
