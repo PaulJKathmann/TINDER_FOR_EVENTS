7947e8708e4f798ddb021aed947f8ead0b8fc128
Alisia Michel <cc0-11@outlook.de>
2019-07-17 10:56:24 +0200

    Method new/create for EventController

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,2 +1,20 @@
 class EventsController < ApplicationController
+  def new
+    @event = Event.new()
+  end
+
+  def create
+    @event = Event.new(event_params)
+    if @event.save
+      redirect_to project_path(@event)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def event_params
+    params.require(:event).permit(:name, :start_date, :end_date, :primary_color, :secondary_color, :logo, :background_image, :welcome_message)
+  end
 end
