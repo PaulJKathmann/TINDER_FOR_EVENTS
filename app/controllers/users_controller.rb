3244f217f9e4c66b13733f50698fb7b8dfc7374b
Paul Kathmann <paul.kathmann.98@gmail.com>
2019-07-18 14:22:45 +0200

    fixed link to swipe page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,7 @@ class UsersController < ApplicationController
   def update
     @user = User.find(current_user.id)
     if @user.save
-      redirect_to swipes_path(params[:id])
+      redirect_to new_swipe_path(params[:id])
     else
       render :edit
     end
