8f2d1b1dafe59b87b1b6774d6dd7899f22b46315
Lu Zhu <luzhu000@gmail.com>
2019-07-24 16:32:47 +0200

    scroll down to last message

--- app/assets/javascripts/scroll.js
+++ app/assets/javascripts/scroll.js
@@ -1,8 +1,10 @@
 function scrollLastMessageIntoView() {
+  console.log("scrollll pls");
   const messages = document.querySelectorAll('.message');
   const lastMessage = messages[messages.length - 1];
-
   if (lastMessage !== undefined) {
     lastMessage.scrollIntoView();
   }
 }
+
+
