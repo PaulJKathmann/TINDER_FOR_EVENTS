804dcd6f06857af5015e7c25a5181cfc62fefe95
Alisia Michel <cc0-11@outlook.de>
2019-07-25 21:00:03 +0200

    margin for layout + tooltip that does not work

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,6 +1,7 @@
 import "bootstrap";
 import "../plugins/flatpickr";
 import {addSwipePageListeners} from "../swipe_card";
-
+import {showEvent} from "../show_event";
 
 addSwipePageListeners();
+showEvent();
